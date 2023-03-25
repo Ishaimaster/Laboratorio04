bcfea957d0232ec8c71242789a3e34049c26b3d9
Ishaimaster <hjflores@correo.url.edu.gt>
2023-03-25 07:48:18 -0600

    s

--- Laboratorio04/Ejercicio03.cs
+++ Laboratorio04/Ejercicio03.cs
@@ -8,9 +8,33 @@ namespace Laboratorio04
 {
     public class Ejercicio03
     {
-        public string Sumar(string num1, string num2) 
+        public string Sumar(string num1, string num2)
         {
-            return "";
+
+            int Suma = 0;
+            int maxLength = Math.Max(num1.Length, num2.Length);
+            StringBuilder result = new StringBuilder(maxLength + 1);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int n1 = i < num1.Length ? num1[num1.Length - 1 - i] - '0' : 0;
+                int n2 = i < num2.Length ? num2[num2.Length - 1 - i] - '0' : 0;
+                int sum = n1 + n2 + Suma;
+                Suma = sum / 10;
+                result.Append(sum % 10);
+            }
+
+            if (Suma > 0)
+            {
+                result.Append(Suma);
+            }
+
+            char[] charArray = result.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
+
     }
+
+    
 }
--- Laboratorio04/Ejercicio04.cs
+++ Laboratorio04/Ejercicio04.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,21 @@ namespace Laboratorio04
     {
         public int[] CalcularBilletes(int cantidad, int[] billetes)
         {
-            return new int[] {};
+            int[] resultado = new int[billetes.Length];
+
+            for (int i = 0; i < billetes.Length; i++)
+            {
+                
+                int cantidadBilletes = cantidad / billetes[i];
+
+                
+                resultado[i] = cantidadBilletes;
+
+                
+                cantidad = cantidad - cantidadBilletes * billetes[i];
+            }
+
+            return resultado;
         }
     }
 }
